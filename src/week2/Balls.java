85fef68d5cc3e6b9b2dd6c40d9170abb7c7cf4be
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2022-08-18 14:33:05 +0200

    working on week 2

---
+++ src/week2/Balls.java
@@ -0,0 +1,10 @@
+package week2;
+
+public class Balls {
+    String text;
+
+    public Balls(String text) {
+        this.text = text;
+    }
+
+}
--- src/week2/RandomizedQueue.java
+++ src/week2/RandomizedQueue.java
@@ -1,14 +1,16 @@
 package week2;
 
-import java.util.Iterator;
 import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    Node first, last;
+    int first, last;
 
     //must support constant amortized time for each queue operation
-    Item [] array = new Item[1];
+    Item[] array;
 
     int N;
 
@@ -19,63 +21,118 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-
+        array = (Item[]) new Object[4];
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return first == null;
+        return N == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return N;
+        return last - first;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+        //grow the array when needed
+        array[last++] = item;
+        for (Item value : array) {
+            System.out.println("array[i] = " + value);
+        }
         N++;
-        Node oldLast = last;
-        last = new Node();
-        last.next = null;
-        last.item = item;
-        if (isEmpty()) first = last;
-        else oldLast.next = last;
     }
 
     // remove and return a random item
     public Item dequeue() {
 
-        int random = StdRandom.uniform(N);
+        if (isEmpty()) throw new NoSuchElementException();
 
-        return Item;
+        /*
+       https://www.coursera.org/learn/algorithms-part1/discussions/forums/jlI-WTmaEeaA1Q4yX3ldsQ/threads/OdA9evssEeaYlAo23CV3mg
+         */
+        return array[0];
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        
+        if (isEmpty()) throw new NoSuchElementException();
+
+        return array[0];
     }
+
+
+    int currentIndex = 0;
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
 
+
+        //TODO shuffle only non-nulls and iterate only over non-nulls
+        StdRandom.shuffle(array);
+
         return new Iterator<Item>() {
             @Override
             public boolean hasNext() {
-                return false;
+                return N > 0;
             }
 
             @Override
             public Item next() {
-                return null;
+                if (isEmpty()) throw new NoSuchElementException();
+
+                //TODO make sure I'm not trying to acess a null here
+                return array[N--];
+            }
+
+            @Override
+            public void remove() {
+                Iterator.super.remove();
+                throw new UnsupportedOperationException();
             }
         };
-
     }
 
     // unit testing (required)
     public static void main(String[] args) {
 
+        RandomizedQueue<Balls> queue = new RandomizedQueue<>();
+
+        queue.enqueue(new Balls("my balls"));
+        queue.enqueue(new Balls("my balls2"));
+        queue.enqueue(new Balls("my balls3"));
+//        queue.enqueue(new Balls("my balls4"));
+
+        for (Balls balls : queue) {
+            System.out.println("item 1 = " + balls.text);
+        }
+
     }
+
+
+
+    /*
+
+    1. Either enqueue or dequeue has to shuffle the items
+        1a. in order to get constant amortized time we could keep track of the number of randomized items, dequeue them
+        and when they end we can shuffle the rest of the items again.
+
+
+                        	Randomized Queue
+Non-iterator operations		Constant amortized time
+Iterator constructor		linear in current # of items
+Other iterator operations	Constant worst-case time
+Non-iterator memory use 	Linear in current # of items
+Memory per iterator	Constant	Linear in current # of items
+     */
+
+
+
+    /*
+
+    TODO ADD the array based queue implemenation to Anki
+     */
 
 }
