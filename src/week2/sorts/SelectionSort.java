c1f8514b98fdc55e8cefab3de42b324c38e78232
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2022-09-01 23:19:24 +0100

    changes

---
+++ src/week2/sorts/SelectionSort.java
@@ -0,0 +1,30 @@
+package week2.sorts;
+
+public class SelectionSort {
+
+
+    public static <T> void sort(Comparable<T>[] a) {
+
+        int N = a.length; //initialize N to the length of the array
+        for (int i = 0; i < N; i++) {
+            int min = i; // assume initially that i is the index of the smallest element
+            for (int j = i + 1; j < N; j++) { // iterate throught the unsorted part
+                if(less(a[j], a[min])) // if we find an element that's smaller then the one we have in min right now
+                    min = j; // update the position of min
+            }
+            exch(a, i, min); //swap the min with what we have in i right now
+        }
+
+    }
+
+
+    private static <T> boolean less(Comparable<T> v, Comparable<T> w) {
+        return v.compareTo(w);
+    }
+
+    private static <T> void exch(Comparable<T>[] a, int i, int j) {
+        Comparable<T> temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
