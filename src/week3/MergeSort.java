6255934717d142d6c7af390f93bb891f896dbec9
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2022-09-25 00:40:54 +0100

    working on week 3 assignment

--- src/week3/MergeSort.java
+++ src/week3/MergeSort.java
@@ -2,17 +2,21 @@ package week3;
 
 public class MergeSort {
 
+
+    private static Comparable [] aux;
+
     private static<T> void sort(Comparable<T>[] a, Comparable<T>[] aux, int lo, int hi) {
 
-        if (hi <= lo) return;
+        if (lo >= hi) return;
         int mid = lo + (hi - lo) / 2;
         sort(a, aux, lo, mid);
         sort(a, aux, mid + 1, hi);
+        if (less(a[mid], a[mid + 1])) return;
         merge(a, aux, lo, mid, hi);
     }
 
     public static<T> void sort(Comparable<T>[] a) {
-        Comparable<T>[] aux = new Comparable[a.length];
+        aux = new Comparable[a.length];
         sort(a, aux, 0, a.length - 1);
     }
 
---
+++ src/week3/collinear_points/BruteCollinearPoints.java
@@ -0,0 +1,19 @@
+package week3.collinear_points;
+
+public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 points
+    }
+    public int numberOfSegments() {
+        // the number of line segments
+        return 0;
+    }
+    public LineSegment[] segments() {
+        return new LineSegment[2];
+    }                // the line segments
+
+    public static void main(String[] args) {
+
+    }
+}
